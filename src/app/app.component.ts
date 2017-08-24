a8bc04a2850c6db445c0c1042ebef3411020684e
Manoj Chalode <chalodem@gmail.com>
2017-08-24 02:20:00 +0100

    added delete support and logs support with multi user functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,24 +8,69 @@ import { Expense } from "./models/expense";
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    private user: string = "";
     private expenses: Expense[] = [];
     private newExpense: Expense = new Expense();
     private valid: boolean = true;
+    private dbList: FirebaseListObservable<any[]>;
 
     constructor(private _db: AngularFireDatabase) {
-        this._db.list('/expenses').subscribe((response: Expense[]) => this.expenses = response);
-    }
-
-    private save() {
-        Object.keys(this.newExpense).forEach((key: string) => this.valid = !!this.newExpense[key] && this.valid);
-        this._db.list('/expenses').push(this.newExpense).then((response: any) => console.log(response)).catch((error: any) => console.log(error));
-    }
-
-    private update(expense: Expense) {
-        let valid: boolean = true;
-        Object.keys(expense).forEach((key: string) => valid = !!expense[key] && valid);
-        if (valid) {
-            this._db.list('/expenses').set(expense.$key, expense).then((response: any) => console.log(response)).catch((error: any) => console.log(error));
+        let user: string = location.search;
+        if (user.indexOf('?user=') >= 0) {
+            this.user = user.split('?user=')[1];
         }
+        if (this.user) {
+
+            this.dbList = this._db.list(`/${this.user.trim().toLowerCase()}/expenses`);
+            this.dbList.subscribe((response: Expense[]) => {
+                if (response && response.length) {
+                    this.expenses = response.filter((item: Expense) => {
+                        return !item.isDeleted;
+                    });
+                }
+            });
+        }
+    }
+
+    private updateUser(event: any): void {
+        if (!event.target.value) {
+            return;
+        }
+        location.search = `user=${event.target.value.toLowerCase()}`;
+    }
+
+    private save(): void {
+        if (!Expense.validate(this.newExpense)) {
+            return;
+        }
+        this.newExpense.addedOn = new Date().toLocaleString();
+        this.newExpense.updatedOn = new Date().toLocaleString();
+        this.newExpense.isDeleted = false;
+        this.dbList.push(this.newExpense).then((response: any) => {
+            this.newExpense.$key = response.key;
+            this.saveLogs(this.newExpense);
+            this.newExpense = new Expense();
+        }).catch((error: any) => console.log(error));
+    }
+
+    private update(expense: Expense): void {
+        if (Expense.validate(expense)) {
+            expense.updatedOn = new Date().toLocaleString();
+            expense.isDeleted = false;
+            this.dbList.set(expense.$key, expense).then((response: any) => this.saveLogs(expense)).catch((error: any) => console.log(error));
+        }
+    }
+
+    private delete(expense: Expense): void {
+        if (!expense || !expense.$key) {
+            return;
+        }
+        expense.isDeleted = true;
+        // this.dbList.remove(expense.$key).then((response: any) => console.log(response)).catch((error: any) => console.log(error));
+        this.dbList.set(expense.$key, expense).then((response: any) => this.saveLogs(expense)).catch((error: any) => console.log(error));
+    }
+
+    private saveLogs(expense: Expense): void {
+        this._db.list(`/${this.user.trim().toLowerCase()}/logs/${expense.$key}`).push(Expense.getLogs(expense));
     }
 }
--- src/app/models/expense.ts
+++ src/app/models/expense.ts
@@ -3,4 +3,18 @@ export class Expense {
     public name : string;
     public amount : string;
     public description : string;
+    public addedOn: string;
+    public updatedOn: string;
+    public isDeleted: boolean;
+
+    public static validate(expense: Expense): boolean {
+        return !!expense.name && !!expense.amount && !!expense.description;
+    }
+
+    public static getLogs(expense: Expense): string {
+        if (!Expense.validate(expense)) {
+            return null;
+        }
+        return `Name - ${expense.name}\nAmount - ${expense.amount}\nDescription - ${expense.description}\nAdded On ${expense.addedOn}\nUpdated On - ${expense.updatedOn}\nDeleted - ${expense.isDeleted}`;
+    }
 }
